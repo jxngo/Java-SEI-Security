9f469b5d80bb212d7df0c50557a4a93af5f32624
Johnny Ngo <johnnyhngo@yahoo.com>
2022-10-17 18:15:29 -0400

    init

---
+++ R09_LCK04_J.java
@@ -0,0 +1,16 @@
+public class R09_LCK04_J {
+    private final Map<Integer, String> mapView =
+            Collections.synchronizedMap(new HashMap<Integer, String>());
+    private final Set<Integer> setView = mapView.keySet();
+ 
+    public Map<Integer, String> getMap() {
+        return mapView;
+    }
+    public void doSomething() { 
+        synchronized (setView) {  // Incorrectly synchronizes on setView
+            for (Integer k : setView) {
+                // ...
+            }
+        }
+    }
+}
